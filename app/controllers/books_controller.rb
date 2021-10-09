672658f117053177064f320370877259ae20b646
EC2 Default User <ec2-user@ip-172-31-88-18.ec2.internal>
2021-10-09 10:05:14 +0000

    [Add]css設定開始

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,19 @@
 class BooksController < ApplicationController
 
-  def create
-    @book=Book.new(book_params)
-    @book.save
-    flash[:notice]="Book was successfully created."
-    redirect_to book_path(@book.id)
+  def index
+    @books=Book.all
+    @book=Book.new
   end
 
-  def index
-    @book=Book.new
-    @books=Book.all
+  def create
+    @book=Book.new(book_params)
+    if @book.save
+      redirect_to 
+      flash[:notice]="Book was successfully created."
+    else
+      @books=Book.all
+      render :index
+    end
   end
 
   def show
@@ -22,9 +26,12 @@ class BooksController < ApplicationController
 
   def update
     @book=Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice]="Book was successfully updated."
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+     flash[:notice]="Book was successfully updated."
+     redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
